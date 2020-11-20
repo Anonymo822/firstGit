c1c8a23f96554e0888e0b9454669f40795126b12
Anonymo822 <delphin.samain@gmail.com>
2020-11-20 14:47:52 +0100

    Premier git

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string test = Console.ReadLine();
+            Console.WriteLine(test);
+
+        }
+    }
+}
